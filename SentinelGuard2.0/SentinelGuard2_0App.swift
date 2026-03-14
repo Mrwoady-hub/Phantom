e285c61348c812df0f7a8ecccb66ec2d2f24bc26
George Gonzalez <Mr.Woady@gmail.com>
2026-03-14 12:28:09 -0400

    Initial Commit

---
+++ SentinelGuard2.0/SentinelGuard2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  SentinelGuard2_0App.swift
+//  SentinelGuard2.0
+//
+//  Created by George Gonzalez on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct SentinelGuard2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
